b3c43ee9c3fc15644abd1d38fa81b9fd6a99893b
Guillermo <gbustamante21@alumnos.utalca.cl>
2023-11-21 19:57:40 -0300

    Preparando para deploy

--- src/frontend/src/API.js
+++ src/frontend/src/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const url = "http://localhost:8080/api/"
-
+const url ="https://tsunamibank-backend.onrender.com/api/"
+//const url = "http://localhost:8080/api/"
 
 
 export default class API {
