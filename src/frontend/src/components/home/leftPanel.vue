196d5616ba86b3ae43069d0d2b5b99c557409303
Carlos Jara <cjara447@gmail.com>
2023-12-16 20:25:16 -0300

    feat(leftPanel.vue , mainPanel.vue , home.vue) Arreglo MainPanel , Seleccion de cuentas en leftPanel
    Se cambian los botones a los de Vuetify, se reordenan elementos del LeftPanel y el mainPanel, se crea menú despegable para seleccion del tipo de cuenta , se crean emits y props para la comunicacion entre leftpanel y mainPanel , Tiempo 2:20hrs.

--- src/frontend/src/components/home/leftPanel.vue
+++ src/frontend/src/components/home/leftPanel.vue
@@ -1,58 +1,88 @@
 <script setup>
-    import Button from 'primevue/button';
-    
+    import {defineEmits} from 'vue';
+    const emit = defineEmits();
+    const menuCuenta =(tipo)=>{
+        console.log("Cuenta de tipo:",tipo)
+        emit('tipo',tipo)
+    };
+    const tipoCuenta=[
+        { id: 1 , cuenta: 'Cuenta Corriente' },
+        { id: 2 , cuenta: 'Cuenta Ahorro' },
+        { id: 3 , cuenta: 'Cuenta Vista' },
+    ];
+
 </script>
 
 <template>
     <div class="primaldiv">
         <card class="contenedor">
             <div>
-                <div>
-                    <img src="../icons/pitufo.jpg" class="icono">
-                    <Button class="boton" label="Inicio" :ripple="false"/>
-                    <hr class="linea">
+                <div class="d-flex align-center" style="border-bottom: 2px solid #ffff">
+                    <img src="../icons/Pitufo.jpg" class="icono">
+                    <v-col>
+                        <v-btn size="x-large" variant="text" style="color:#ffff; width:100%">
+                            Inicio
+                        </v-btn>
+                    </v-col>
                 </div>
-                
-                <div>
+                <div class="d-flex align-center" style="border-bottom: 2px solid #ffff">
                     <img src="../icons/Cuentas.jpg" class="icono">
-                    <Button class="boton" label="Cuenta" />
-                    <hr class="linea">
+                    <v-col>
+                        <v-menu
+                            location="end"
+                            open-on-hover
+                        >
+                        <template v-slot:activator="{ props }">
+                            <v-btn
+                            class="boton" size="x-large" variant="text" style="color:#ffff ;width:100%"
+                            v-bind="props"
+                            >
+                            Cuentas
+                            </v-btn>
+                        </template>
+                        <v-list
+                        style="color: #0039A6;"
+                        >
+                            <v-list-item
+                            v-for="(item, index) in tipoCuenta"
+                            :key="index"
+                            >
+                            <v-btn variant="text" size="x-large" style="width:100%;" @click="menuCuenta(item)">{{ item.cuenta }}</v-btn>
+                            </v-list-item>
+                        </v-list>
+                        </v-menu>
+                    </v-col>
                 </div>
-                
-                <div>
+                <div class="d-flex align-center" style="border-bottom: 2px solid #ffff; width:100%">
                     <img src="../icons/Bob.jpg" class="icono">
-                    <Button class="boton" label="Retiros" />
-                    <hr class="linea">
+                    <v-col>
+                        <v-btn size="x-large"  variant="text" style="color:#ffff; width:100%">
+                            Retiros
+                        </v-btn>
+                    </v-col>
                 </div>
-                
-                <div>
+                <div class="d-flex align-center" style="border-bottom: 2px solid #ffff">
                     <img src="../icons/Deposito.jpg" class="icono">
-                    <Button class="boton" label="Depositos" />
-                    <hr class="linea">
+                    <v-col>
+                        <v-btn size="x-large"  variant="text" style="color:#ffff;width:100%">
+                            Depositos
+                        </v-btn>
+                    </v-col>
                 </div>
-
-                <div>
+                <div class="d-flex align-center" style="border-bottom: 2px solid #ffff">
                     <img src="../icons/Comision.jpg" class="icono">
-                    <Button class="boton" label="Comisiones" />
-                    <hr class="linea">
+                    <v-col>
+                        <v-btn size="x-large" variant="text" style="color:#ffff;width:100%">
+                            Comisiones
+                        </v-btn>
+                    </v-col>
                 </div>
-            
             </div>
         </card>
     </div> 
 </template>
 
 <style>
-
-    .boton{
-        color:#ffffff;
-        background-color: transparent;
-        border-color: transparent;
-        font-size: 18px;
-        margin-bottom: 20px;
-        margin-left: 10px;
-    }
-
     .contenedor{
         background-color: #D52B1E;
         height: 100%;
@@ -69,9 +99,5 @@
         width: 100%; 
         display: flex; 
         align-items: left;   
-    }
-    .linea{
-        width: 100%;
-        margin-bottom: 1%;
     }
 </style>
